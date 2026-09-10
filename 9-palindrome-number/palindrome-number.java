36bebe490ec8de5aa63895d292ea5b8cb3fe9a98
visheshpundir2186-svg <visheshpundir2186@gmail.com>
2026-09-10 22:08:09 +0530

    Time: 5 ms (82.52%) | Memory: 46.1 MB (32.80%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int revno = 0;
+
+        while (x > 0) {
+            int lastdigit = x % 10;
+            x = x / 10;
+            revno = revno * 10 + lastdigit;
+        }
+
+        return original == revno;
+    }
+}
+    
